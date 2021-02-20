4102d9c0652639b393d07d40a12590b1abd05d16
Andrija Hanga <a.hanga@student.fontys.nl>
2021-02-20 21:36:04 +0100

    Working Add Task, Change Task, Delete Task funcionts.

--- ToDoProject/Form1.cs
+++ ToDoProject/Form1.cs
@@ -12,9 +12,58 @@ namespace ToDoProject
 {
     public partial class Form1 : Form
     {
+        private Tasks tasks;
         public Form1()
         {
             InitializeComponent();
+            tasks = new Tasks();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (var task in tasks.AddTask(textBox1.Text))
+            {
+                listBox1.Items.Add(task);
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            if (listBox1.SelectedIndex != -1)
+            {
+                tasks.DeleteTask(listBox1.SelectedIndex);
+                listBox1.Items.Clear();
+                foreach (var task in tasks.GetTasks())
+                {
+                    listBox1.Items.Add(task);
+                }
+                            
+            }
+          
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                tasks.ChangeTask(listBox1.SelectedIndex, textBox2.Text);
+                listBox1.Items.Clear();
+                foreach (var task in tasks.GetTasks())
+                {
+                    
+                    listBox1.Items.Add(task);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
         }
     }
-}
+    }
+
---
+++ ToDoProject/Tasks.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoProject
+{
+    class Tasks
+    {
+        private static List<string> tasks = new List<string>();
+        private static List<string> doneTasks = new List<string>();
+
+
+        public List<string> AddTask(string task)
+        {
+            tasks.Add(task);
+            return tasks;
+        }
+        public void DeleteTask(int index)
+        {
+            tasks.RemoveAt(index);
+        }
+        public void ChangeTask(int index, string task)
+        {
+            tasks[index] = task;
+        }
+        public void CompleteTask(int index)
+        {
+            List<string> doneTasks = new List<string>();
+            foreach (var task in tasks)
+            {
+                tasks.AddRange(doneTasks);
+            }
+            
+        }
+        public List<string> GetTasks()
+        {
+            return tasks;
+        }
+      /*  public string GetTask(string task)
+        {
+            return task;
+
+        }      */
+    }
+
+}
